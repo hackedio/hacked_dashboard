08196b0431d9bc2bacb6d751cafba063d02495a3
SeanHolden <sean.holden@o2.com>
2013-07-08 17:14:08 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackedDashboard::Application.config.secret_key_base = 'c3f284762b2039cb0e8a61d70e23d7f61f69fc99e767f78938c4942c81435eb5763692c213e7ea4c9d686e03eaac10c5988c6217391c591f812eb0cc129dc79f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackedDashboard::Application.config.session_store :cookie_store, key: '_hacked_dashboard_session'
